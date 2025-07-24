cd3f22388d427535608891dae2df7de63c828b74
viasanro <victor.santos@fpuna.edu.py>
2025-07-23 23:47:01 -0300

    utils directory created

---
+++ utils/modelado/python/Cz_Cz_Embotelladora_DimCatalogoSpmk.py
@@ -0,0 +1,194 @@
+# Databricks notebook source
+from pyspark.sql.functions import md5, concat, col
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC refresh table embotelladora.tbl_catalogo_instock;
+# MAGIC refresh table embotelladora.tmp_catalogo_instock;
+
+# COMMAND ----------
+
+df_catalogo_spmk = spark.sql("""
+                          select distinct concat(lower(trim(bandera)), '-', lower(trim(cadena)), '-', codcadena, '-', codigoabi) as key_catalogo
+                            ,activocodigoabi as activo_cod_abi
+                            ,bandera
+                            ,cadena
+                            ,codcadena as codigo_cadena
+                            ,codigoabi as codigo_abi
+                            ,catalogado
+                            ,cast(getdate() as date) as fecha_ini
+                            ,'9999-12-31' as fecha_fin
+                            ,'S' as status
+                          from embotelladora.tbl_catalogo_instock
+                        """)
+df_catalogo_spmk = df_catalogo_spmk.withColumn("checksum", md5(concat(df_catalogo_spmk.bandera, df_catalogo_spmk.cadena, df_catalogo_spmk.codigo_cadena, df_catalogo_spmk.codigo_abi, df_catalogo_spmk.activo_cod_abi, df_catalogo_spmk.catalogado)))
+df_catalogo_spmk.createOrReplaceTempView('AuxSourceCatalogoSpmk')
+
+# COMMAND ----------
+
+# DBTITLE 1,Change Data Capture
+try:
+  path_dim_catalogo = '/mnt/consumezone/Chile/Logistica/Embotelladora/DimCatalogoSpmk'
+  df_dim_catalogo = spark.read.format('parquet').load(path_dim_catalogo)
+  df_dim_catalogo.createOrReplaceTempView('DimCatalogoSpmk')
+
+  source_catalogo_spmk = spark.sql("""
+                                     -- Registros nuevos a insertar
+                                        select c.* 
+                                        from AuxSourceCatalogoSpmk c
+                                        where not exists (select * from DimCatalogoSpmk d where c.key_catalogo = d.key_catalogo)
+                                        union
+                                    -- Registros modificados a insertar
+                                        select c.* 
+                                        from AuxSourceCatalogoSpmk c
+                                        join DimCatalogoSpmk d on c.key_catalogo = d.key_catalogo
+                                        and c.checksum != d.checksum
+                                        and d.status = 'S'
+                                        union
+                                    -- Registros a actualizar y sobreescribir en la Dim
+                                        select d.key_catalogo
+                                              ,d.activo_cod_abi 
+                                              ,d.bandera 
+                                              ,d.cadena 
+                                              ,d.codigo_cadena 
+                                              ,d.codigo_abi
+                                              ,d.catalogado 
+                                              ,d.fecha_ini
+                                              ,c.fecha_ini as fecha_fin
+                                              ,'N' as status
+                                              ,d.checksum
+                                        from AuxSourceCatalogoSpmk c
+                                        join DimCatalogoSpmk d on c.key_catalogo = d.key_catalogo
+                                        and c.checksum != d.checksum
+                                        and d.status = 'S'
+                                    """)
+except Exception as e:
+  if 'Path does not exist' in str(e):
+    source_catalogo_spmk = df_catalogo_spmk.select("*")
+
+source_catalogo_spmk.createOrReplaceTempView('SourceCatalogoSpmk')
+
+# COMMAND ----------
+
+# DBTITLE 1,Nueva Dim Sin PK
+try:
+  path_dim_catalogo = '/mnt/consumezone/Chile/Logistica/Embotelladora/DimCatalogoSpmk'
+  df_dim_catalogo = spark.read.format('parquet').load(path_dim_catalogo)
+  df_dim_catalogo.createOrReplaceTempView('DimCatalogoSpmk')
+
+  df_dim_catalogo_sin_pk = spark.sql("""
+                                     select coalesce(c.key_catalogo, d.key_catalogo)      as key_catalogo
+                                            ,coalesce(c.activo_cod_abi, d.activo_cod_abi) as activo_cod_abi
+                                            ,coalesce(c.bandera, d.bandera)               as bandera
+                                            ,coalesce(c.cadena, d.cadena)                 as cadena
+                                            ,coalesce(c.codigo_cadena, d.codigo_cadena)   as codigo_cadena 
+                                            ,coalesce(c.codigo_abi, d.codigo_abi)         as codigo_abi
+                                            ,coalesce(c.catalogado, d.catalogado)         as catalogado
+                                            ,coalesce(c.fecha_ini, d.fecha_ini)           as fecha_ini
+                                            ,coalesce(c.fecha_fin, d.fecha_fin)           as fecha_fin
+                                            ,coalesce(c.status, d.status)                 as status   
+                                            ,coalesce(c.checksum, d.checksum)             as checksum
+                                        from SourceCatalogoSpmk c
+                                        full join DimCatalogoSpmk d 
+                                        on c.key_catalogo = d.key_catalogo
+                                    """)
+except Exception as e:
+  if 'Path does not exist' in str(e):
+    df_dim_catalogo_sin_pk = df_catalogo_spmk.select("*")
+
+df_dim_catalogo_sin_pk.createOrReplaceTempView('DimCatalogoSpmkSinPk')
+
+# COMMAND ----------
+
+# DBTITLE 1,Agregamos la PK a la Dim
+try:
+  dim_catalogo_spmk = spark.sql("""
+                            SELECT CAST(coalesce(dim.pk_catalogo, sum(1) OVER (ORDER BY dim.key_catalogo ROWS UNBOUNDED PRECEDING ) + cntrl.maxid) AS int) AS pk_catalogo
+                                  ,csk.key_catalogo
+                                  ,csk.activo_cod_abi
+                                  ,csk.bandera
+                                  ,csk.cadena
+                                  ,csk.codigo_cadena
+                                  ,csk.codigo_abi
+                                  ,csk.catalogado
+                                  ,csk.fecha_ini
+                                  ,csk.fecha_fin
+                                  ,csk.status
+                                  ,csk.checksum
+                                  ,CAST(getdate() AS date) AS last_update
+                            FROM DimCatalogoSpmkSinPk csk
+                            CROSS JOIN (SELECT coalesce(max(cast(pk_catalogo as int)), 0) AS maxid FROM DimCatalogoSpmk) cntrl
+                            LEFT JOIN DimCatalogoSpmk dim
+                            ON csk.key_catalogo = dim.key_catalogo
+                            WHERE dim.key_catalogo != 'DESCONOCIDO'
+                            UNION
+                            SELECT -1 AS pk_catalogo
+                                  ,'DESCONOCIDO' AS key_catalogo
+                                  ,'-1' AS activo_cod_abi
+                                  ,'DESCONOCIDO' AS bandera
+                                  ,'DESCONOCIDO' AS cadena
+                                  ,'-1' AS codigo_cadena
+                                  ,'-1' AS codigo_abi
+                                  ,'-1' AS catalogado
+                                  ,CAST(getdate() AS date) AS fecha_ini
+                                  ,'9999-12-31' AS fecha_fin
+                                  ,'S' AS status
+                                  ,'DESCONOCIDO' AS checksum
+                                  ,CAST(getdate() AS date) AS last_update """)
+except Exception as e:
+  dim_catalogo_spmk = spark.sql("""
+                            SELECT CAST(sum(1) OVER (ORDER BY csk.key_catalogo ROWS UNBOUNDED PRECEDING ) AS int) AS pk_catalogo
+                                  ,csk.key_catalogo
+                                  ,csk.activo_cod_abi
+                                  ,csk.bandera
+                                  ,csk.cadena
+                                  ,csk.codigo_cadena
+                                  ,csk.codigo_abi
+                                  ,csk.catalogado
+                                  ,csk.fecha_ini
+                                  ,csk.fecha_fin
+                                  ,csk.status
+                                  ,csk.checksum
+                                  ,CAST(getdate() AS date) AS last_update
+                            FROM DimCatalogoSpmkSinPk csk
+                            UNION
+                            SELECT -1 AS pk_catalogo
+                                  ,'DESCONOCIDO' AS key_catalogo
+                                  ,'-1' AS activo_cod_abi
+                                  ,'DESCONOCIDO' AS bandera
+                                  ,'DESCONOCIDO' AS cadena
+                                  ,'-1' AS codigo_cadena
+                                  ,'-1' AS codigo_abi
+                                  ,'-1' AS catalogado
+                                  ,CAST(getdate() AS date) AS fecha_ini
+                                  ,'9999-12-31' AS fecha_fin
+                                  ,'S' AS status
+                                  ,'DESCONOCIDO' AS checksum
+                                  ,CAST(getdate() AS date) AS last_update """)
+
+# COMMAND ----------
+
+dim_catalogo_spmk.display()
+
+# COMMAND ----------
+
+try:
+  # Saving on DL
+  cz_path = '/mnt/consumezone/Chile/Logistica/Embotelladora/DimCatalogoSpmk'
+  dim_catalogo_spmk.write.mode('overwrite').format('parquet').save(cz_path)
+except Exception as e:
+  raise e
+
+# COMMAND ----------
+
+dbutils.notebook.run("/LAS/Templates/Cz_Presto_Template_Db_V2",300,
+                     {"prestoTable":"dim_catalogo_spmk", 
+                      "pathCz":"/consumezone/Chile/Logistica/Embotelladora/DimCatalogoSpmk", 
+                      "prestoSchema":"ch_logistica",
+                      "partitionByField":"-", 
+                      "tipoCarga":"FULL"})
+
+# COMMAND ----------
+
+spark.read.format('parquet').load('/mnt/consumezone/Chile/Logistica/Embotelladora/DimCatalogoSpmk').display()
---
+++ utils/sharepoint/Sharepoint_Management.py
@@ -0,0 +1,84 @@
+# Databricks notebook source
+from office365.runtime.auth.authentication_context import ClientCredential
+from office365.sharepoint.client_context import ClientContext
+from office365.sharepoint.files.file import File
+
+# COMMAND ----------
+
+class SharepointManagement():
+  """Clase para administrar (listar, bajar, subir) archivos de Sharepoint."""
+  def __init__(self, tenant_name, site, subfolder, secretuser, secretpass):
+    """Inicializa la clase con los parametros necesarios para conectarse a Sharepoint.
+    
+    Args:
+        tenant_name (str): Nombre del inquilino de Sharepoint (ejemplo: <mytenantname>)
+        site (str): Nombre del sitio de Sharepoint (ejemplo: <mysitename>)
+        subfolder (str): Nombre del subdirectorio donde se almacenaran los archivos (ejemplo: <subdirectoriodentrodelsite>)
+        secretuser (str): Nombre del secret de Azure Key Vault con el usuario para conectarse al sitio (ejemplo: <secretuser>)
+        secretpass (str): Nombre del secret de Azure Key Vault con la contraseña para conectarse al sitio (ejemplo: <secretpassword>)
+    """
+    self.tenant_name = tenant_name
+    self.site = site
+    self.user = dbutils.secrets.get(scope = 'KeyVault', key = secretuser)
+    self.password = dbutils.secrets.get(scope = 'KeyVault', key = secretpass)
+    self.site_url = f'https://{tenant_name}.sharepoint.com/sites/{site}'
+    self.folder_relative_url = f"/sites/{site}/Shared Documents/{subfolder}"
+
+  def setUrlDescarga(self,url_descarga):
+    """Establece la url del archivo a descargar.
+        Args:
+            url_descarga (str): Url del archivo a descargar (ejemplo: /sites/mysitename/Shared%20Documents/subfolder/filename.xlsx)
+    """
+    self.url_descarga = url_descarga
+  
+  def __conectar(self):
+    """Establece la conexion a Sharepoint y devuelve el contexto de conexión."""
+    try:
+      credentials = ClientCredential(self.user, self.password)
+      ctx = ClientContext(self.site_url).with_credentials(credentials)
+      return ctx
+    except Exception as e:
+      return e
+  
+  def listArchivos(self):
+    """Devuelve una lista con los nombres de los archivos contenidos en el subdirectorio establecido en la variable self.folder_relative_url."""
+    try:
+      ctx = self.__conectar()
+    except Exception as e:
+      return e
+    folder = ctx.web.get_folder_by_server_relative_url(self.folder_relative_url)
+    folder_content = folder.files 
+    ctx.load(folder_content)
+    ctx.execute_query()
+    archivos = [item.name for item in folder_content]
+    return archivos
+
+  def downloadArchivo(self, filename):
+    """Descarga el archivo especificado en la variable url_descarga a la ruta especificada por la variable filename.
+        Args:
+            filename (str): Ruta donde se almacenara el archivo (ejemplo: /dbfs/local_disk0/tmp/nombreArchivo.xlsx)
+    """
+    try:
+      ctx = self.__conectar()
+  
+      with open(filename, 'wb') as outputfile:
+        response = File.open_binary(ctx, self.url_descarga )
+        outputfile.write(response.content)
+      
+    except Exception as e:
+      return e
+    
+  
+  def uploadArchivo(self, nombre, contenido):
+    """Sube el archivo con el nombre y contenido especificados a la ruta especificada por la variable self.folder_relative_url.
+        Args:
+            nombre (str): Nombre del archivo (ejemplo: filename.xlsx)
+            contenido (bytes): Datos binarios que representan el contenido del archivo (archivo Excel en nuestro caso)
+    """  
+    try:
+      ctx = self.__conectar()
+      target_folder = self.folder_relative_url
+      target_folder = ctx.web.get_folder_by_server_relative_url(target_folder)
+      target_folder.upload_file(nombre, contenido).execute_query()
+    except Exception as e:
+      return e
